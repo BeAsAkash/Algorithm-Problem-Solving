11d6b492c30cd1da9a61c8561f195940b6f9a1c7
BeAsAkash <kumaraadarsh8318@gmail.com>
2023-01-24 22:21:11 +0530

    Added Input-Time graphs for sorting algos

--- Questions/BubbleSort.cpp
+++ Questions/BubbleSort.cpp
@@ -1,42 +1,46 @@
-#include<iostream>
-#include<ctime>
-#include<cstdlib>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
-    int m=1;
-    while(m==1){
+    int m = 1;
+    while (m == 1)
+    {
 
-    int n;
-    cout<<"Enter the no of INPUTS : ";
-    cin>>n;
+        int n;
+        cout << "Enter the no of INPUTS : ";
+        cin >> n;
 
-    int arr[n];
+        int arr[n];
 
-    for(int i=0;i<n;i++){
-        arr[i]=rand();
-    }
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = rand();
+        }
 
-    clock_t start_time=clock();
+        clock_t start_time = clock();
 
-    for(int i=0 ; i<n-1 ; i++){
-        for(int j=0 ; j<n-i-1 ; j++){
-			if(arr[j]>arr[j+1]){
-				int temp=arr[j+1];
-				arr[j+1]=arr[j];
-				arr[j]=temp;
-			}
-		}
-    }
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
 
-    clock_t end_time=clock();
+        clock_t end_time = clock();
 
-    cout<<end_time-start_time<<"secs"<<endl;
+        cout << end_time - start_time << "secs" << endl;
 
-    cout<<"Press 0 TO EXIT else press 1 :";
-    cin>>m;
-
+        cout << "Press 0 TO EXIT else press 1 :";
+        cin >> m;
     }
     return 0;
 }
--- Questions/QuickSort.cpp
+++ Questions/QuickSort.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
-int Partition(int A[], int low, int high){
+int Partition(int A[], int low, int high)
+{
     int pivot = A[low];
     int i = low + 1;
     int j = high;
 
-    while (true){
-        while (i <= j && A[i] <= pivot){
+    while (true)
+    {
+        while (i <= j && A[i] <= pivot)
+        {
             i++;
         }
-        while (A[j] >= pivot && j >= i){
+        while (A[j] >= pivot && j >= i)
+        {
             j--;
         }
-        if (j < i){
+        if (j < i)
+        {
             break;
-        } else {
+        }
+        else
+        {
             swap(A[i], A[j]);
         }
     }
@@ -23,39 +32,44 @@ int Partition(int A[], int low, int high){
     return j;
 }
 
-void Quick_sort(int A[], int low, int high){
-    if (low < high){
+void Quick_sort(int A[], int low, int high)
+{
+    if (low < high)
+    {
         int p = Partition(A, low, high);
-        Quick_sort(A, low, p-1);
-        Quick_sort(A, p+1, high);
+        Quick_sort(A, low, p - 1);
+        Quick_sort(A, p + 1, high);
     }
 }
 
-int main(){
+int main()
+{
 
-int m=1;
-    while(m==1){
+    int m = 1;
+    while (m == 1)
+    {
 
-    int n;
-    cout<<"Enter the no of INPUTS : ";
-    cin>>n;
+        int n;
+        cout << "Enter the no of INPUTS : ";
+        cin >> n;
 
-    int arr[n];
+        int arr[n];
 
-    for(int i=0;i<n;i++){
-        arr[i]=rand();
-    }
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = rand();
+        }
 
-    clock_t start_time=clock();
-    Quick_sort(arr,0,(sizeof(arr)/4));
+        clock_t start_time = clock();
 
-     clock_t end_time=clock();
+        Quick_sort(arr, 0, (sizeof(arr) / 4));
 
-    cout<<end_time-start_time<<"secs"<<endl;
+        clock_t end_time = clock();
 
-    cout<<"Press 0 TO EXIT else press 1 :";
-    cin>>m;
+        cout << end_time - start_time << "secs" << endl;
 
+        cout << "Press 0 TO EXIT else press 1 :";
+        cin >> m;
     }
 
     return 0;
